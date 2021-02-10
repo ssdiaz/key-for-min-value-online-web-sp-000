2e968bcc13c4f9c6f5d21b619761ca9155defc3b
Sam D <samidiaz3@gmail.com>
2021-02-10 18:05:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,14 @@
 # returns key of smalled value of set
 def key_for_min_value(name_hash)
   name_of_smallest = nil
-  smallest_value = ""
+  smallest_value = nil
+
   name_hash.each do |name, num|
-    smallest_value = num
-    if num <= smallest_value
+    current_num = num
+    if smallest_value == nil || current_num <= smallest_value
+      smallest_value = num
       name_of_smallest = name
-     end
+    end
   end
   name_of_smallest
 end
