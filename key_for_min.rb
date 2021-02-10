31872323d1871bdd1e6302eb3375a4375f71a905
Sam D <samidiaz3@gmail.com>
2021-02-10 17:36:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@
 def key_for_min_value(name_hash)
   name_of_smallest = nil
   smallest_value = nil
-  
+
   name_hash.each do |name, num|
-    if num < smallest_value 
+    if num < smallest_value
       name = name_of_smallest
       # puts name
      end
