f8b3b24a16f697652f63a570c9572567e8f6daa0
Sam D <samidiaz3@gmail.com>
2021-02-10 18:10:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,9 @@ def key_for_min_value(name_hash)
   smallest_value = nil
 
   name_hash.each do |name, num|
-    #current_num = num
-    if smallest_value == nil || num <= smallest_value
-      smallest_value = num
-      name_of_smallest = name
+    if smallest_value == nil || num < smallest_value #if first time ran(smallest_value is nil) OR if this num is < smallest_value
+      smallest_value = num                              # store this num as smallest_value
+      name_of_smallest = name                           # store the name for later
     end
   end
   name_of_smallest
